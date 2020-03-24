33b7cb03e1a23e249686f60c808c68c334d53f11
Darren <darren.daley@hotmail.co.uk>
2020-03-24 21:14:51 +0000

    started work on linked list

---
+++ Library/Data Structures/DoubleLinkedList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Data_Structures
+{
+    public class DoubleLinkedList<T>
+    {
+        public int Count { get; private set; }
+
+        public bool IsEmpty => Count == 0;
+
+
+        public DoubleLinkedNode<T> Head { get; private set; }
+
+        public DoubleLinkedNode<T> Tail { get; private set; }
+
+        private void AddFirst(DoubleLinkedNode<T> node)
+        {
+            var temp = Head;
+
+            Head.Next = temp;
+
+            Head = node;
+
+            if (IsEmpty) {
+                Tail = Head;
+            }
+            else {
+                // need to check on this line, too tired to do it now
+                Head.Previous = temp;
+            }
+
+            Count++;
+        }
+    }
+}
---
+++ Library/Data Structures/DoubleLinkedNode.cs
@@ -0,0 +1,15 @@
+﻿namespace Library.Data_Structures
+{
+    public class DoubleLinkedNode<T>
+    {
+        public DoubleLinkedNode<T> Next { get; internal set; }
+
+        public DoubleLinkedNode<T> Previous { get; internal set; }
+
+        public T Value { get; set; }
+
+        public DoubleLinkedNode(T value) {
+            Value = value;
+        }
+    }
+}
